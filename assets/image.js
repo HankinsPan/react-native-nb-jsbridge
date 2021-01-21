c146c08cd970ad30dabeb958f2584aa3f5228954
HankinsPan <hankins.home@outlook.com>
2021-01-21 13:19:32 +0800

    lib build

---
+++ assets/image.js
@@ -0,0 +1,9 @@
+const image = {
+  share: {
+    friend: require('./popup_wechat_btn.png'),
+    network: require('./pouup_circle_btn.png'),
+    poster: require('./popup_poster_btn.png'),
+  },
+}
+
+export default image
